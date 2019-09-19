d002fcfee27857d7b773957843769e036d9e6fc5
Saron <saronnhong@gmail.com>
2019-09-18 22:35:13 -0700

    stories background change to dynamic

--- client/src/components/Stories/Stories.js
+++ client/src/components/Stories/Stories.js
@@ -6,7 +6,7 @@ import "./style.css";
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 class Stories extends Component {
-  
+
   state = {
     title: "",
     author: "",
@@ -17,20 +17,20 @@ class Stories extends Component {
     delete: "",
     id: ""
   };
-  
-  
+
+
 
   componentDidMount() {
-    this.setState({id: this.props.match.params.id}, ()=> this.loadArticles());
-    
-    
+    this.setState({ id: this.props.match.params.id }, () => this.loadArticles());
+
+
   }
 
   loadArticles = () => {
     API.getArticlesById(this.state.id)
       .then(res => {
         console.log(res.data);
-        
+
         this.setState({ articles: res.data });
 
         console.log(this.props.match.params.id);
@@ -38,26 +38,29 @@ class Stories extends Component {
 
       .catch(err => console.log(err));
   };
-  render(){
+  render() {
+    var divStyle = {
+      backgroundImage: 'url(' + this.state.articles.imageUrl + ')'
+    }
 
-  
-  return (
-    <div className="storiesPage">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
+    return (
+      <div className="storiesPage" style={divStyle}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
 
-          </div>
-          <div className="col-md-6 articleSection">
-            <h1 className="artTitle">{this.state.articles.title} </h1>
-            <h6>by {this.state.articles.author} </h6>
-            <h10 className="artBody">{this.state.articles.content}</h10>
+            </div>
+            <div className="col-md-6 articleSection">
+              <h1 className="artTitle">{this.state.articles.title} </h1>
+              <h6>by {this.state.articles.author} </h6>
+              <h10 className="artBody">{this.state.articles.content}</h10>
+            </div>
           </div>
         </div>
-      </div>
 
-    </div>
-  );
-}}
+      </div>
+    );
+  }
+}
 
 export default Stories;
